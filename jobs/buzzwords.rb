842af51fbfc3693c7e30c6ff0311f63922f95448
e-coucou <eric@ecoucou.com>
2014-03-11 23:25:16 +0100

    Dev Master v0.2.0

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,7 +1,16 @@
-buzzwords = ['Action', 'Leverage', 'Pivoting', 'Turn-key', 'Streamlininess', 'Enterprise', 'Web 2.0'] 
+require "rubygems"
+require "roo"
+require "google_drive"
+
+key = "0AsWa_wvrz5AndDBHVS1BbXU1MVJpVDE2VDk3akNJOWc"
+	
+buzzwords = ['Action', 'Pivoting', 'Turn-key', 'Streamlininess', 'Enterprise', 'Web 2.0'] 
 buzzword_counts = Hash.new({ value: 0 })
 
 SCHEDULER.every '2s' do
+  session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
+  ws = session.spreadsheet_by_key(key).worksheets[0]
+
   random_buzzword = buzzwords.sample
   buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
   
