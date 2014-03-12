6f8c170888dac4cd06afbc2fb2c3f9f2c50633b3
e-coucou <eric@ecoucou.com>
2014-03-12 21:28:25 +0100

    Dev Master v0.2.2

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -6,22 +6,23 @@ require "google_drive"
 key = "0AkmmwnTmClhodDBHa2xJSDBtTE5LVF9UYktmOUZTeGc"
 current_karma = 0
 
-SCHEDULER.every '2m' do
+SCHEDULER.every '5s' do
 	# open session to google
 	session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
 	ws = session.spreadsheet_by_key(key).worksheets[0]
 	# get data from google spreadsheet [row, col]
-	current = ws[3, 2].to_f # 3:B
-	last    = ws[3, 3].to_f # 3:B
-	moreinfo = ws[3, 5].to_f # 3:B
-	new_title = ws[3, 4].to_f # 3:B
+p	current = ws[3, 2].to_f # 3:B
+p	last    = ws[3, 3].to_f # 3:C
+p	moreinfo = ws[3, 5].to_s # 3:E
+p	new_title = ws[3, 4].to_s # 3:D
+p	suffix = ws[3, 6].to_s # 3:F
 	
 
 #  last_valuation = current_valuation
   last_karma     = current_karma
   current_karma     = rand(200000)
 
-  send_event('valuation', { current: current, last: last, moreinfo: moreinfo, title: new_title })
+  send_event('e-CNumber', { current: current, last: last, moreinfo: moreinfo, title: new_title, suffix: suffix })
 #  send_event('karma', { current: current_karma, last: last_karma })
   send_event('synergy',   { value: rand(100) })
 end
