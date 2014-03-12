e7fefbeafd6b0cd4ae861f6c1195ff82bdd4be97
e-coucou <eric@ecoucou.com>
2014-03-12 22:11:49 +0100

    Dev Master v0.2.2

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -16,15 +16,14 @@ SCHEDULER.every '5s' do
 	moreinfo = ws[3, 5].to_s # 3:E
 	new_title = ws[3, 4].to_s # 3:D
 	suffix = ws[3, 6].to_s # 3:F
+	prefix = ws[3, 6].to_s # 3:F
 	# -----
 	g_value = ws[4, 2].to_f # 4:B
 	g_max    = ws[4, 3].to_f # 4:C
-	g_moreinfo = ws[4, 5].to_s # 4:E
 	g_new_title = ws[4, 4].to_s # 4:D
-	g_suffix = ws[4, 6].to_s # 4:F
 
-	send_event('e-CNumber', { current: current, last: last, moreinfo: moreinfo, title: new_title, suffix: suffix })
+	send_event('e-CNumber', { current: current, last: last, moreinfo: moreinfo, title: new_title, suffix: suffix, prefix: prefix })
 	send_event('synergy',   { value: rand(100) })
-	send_event('e-CUsageGauge', {	max: g_max,	value: g_value, title: g_new_title, moreinfo: g_moreinfo	})
+	send_event('e-CUsageGauge', {	max: g_max,	value: g_value, title: g_new_title })
 
 end
