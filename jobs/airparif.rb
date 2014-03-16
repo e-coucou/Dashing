aa59df4e15668b97dc69b645eba58c0a89411af2
e-coucou <eric@ecoucou.com>
2014-03-16 13:21:52 +0100

    Test for AirPArif 0.1

--- jobs/airparif.rb
+++ jobs/airparif.rb
@@ -2,25 +2,22 @@ require "rubygems"
 require "roo"
 require "google_drive"
 
-current_valuation = 0
 #key = "0AkHkekTTsBL0dGJ5Ty14YjRud2VBbHhDMXd2UGZPVlE"
 key = "0AsWa_wvrz5AndDBHVS1BbXU1MVJpVDE2VDk3akNJOWc"
 
 SCHEDULER.every '2s' do
-	# Logs in.
-	# You can also use OAuth. See document of
-	# GoogleDrive.login_with_oauth for details.
-	
+	# Logs in.	
 	session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
-	ws = session.spreadsheet_by_key(key).worksheets[1]
+	ws = session.spreadsheet_by_key(key).worksheets[0]
 
-    PM10 = ws[1, 2].to_f   # cellule ligne 1  / colonne 2
-	NO2 = ws[1, 3].to_f		# l/c
-    PM10 = ws[1, 6].to_f   # cellule ligne 1  / colonne 2
-	NO2 = ws[1, 7].to_f		# l/c
+#p  Lieu = ws[1, 2].to_f   # cellule ligne 1  / colonne 2
+#p	Date = ws[1, 3].to_f		# l/c
+#p	Heure = ws[1, 4].to_f		# l/c
+p   c_PM10 = ws[1, 6].to_f   # cellule ligne 1  / colonne 2
+#p	NO2 = ws[1, 7].to_f		# l/c
 
-	send_event('ap-val', { current: PM10, moreinfo: '' })
-	send_event('ap-fo', { current: PM10, moreinfo: '' })
-	send_event('ap-circle', { value: PM10, min: 0, max: 150 , moreinfo: "cible : 10 encours max" })
-	send_event('ap-bg', {	max: PM10,	value: PM10	})
+	send_event('ap-val', { current: c_PM10, moreinfo: '' })
+#	send_event('ap-fo', { current: NO2, moreinfo: '' })
+	send_event('ap-circle', { value: c_PM10, min: 0, max: 150 , moreinfo: "cible : 10 encours max" })
+	send_event('ap-bg', {	max: c_PM10,	value: c_PM10	})
 end
--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -2,14 +2,14 @@ require "rubygems"
 require "roo"
 require "google_drive"
 
-key = "0AsWa_wvrz5AndDBHVS1BbXU1MVJpVDE2VDk3akNJOWc"
+#key = "0AsWa_wvrz5AndDBHVS1BbXU1MVJpVDE2VDk3akNJOWc"
 	
 buzzwords = ['Action', 'Pivoting', 'Turn-key', 'Streamlininess', 'Enterprise', 'Web 2.0'] 
 buzzword_counts = Hash.new({ value: 0 })
 
 SCHEDULER.every '2s' do
-  session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
-  ws = session.spreadsheet_by_key(key).worksheets[0]
+#  session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
+#  ws = session.spreadsheet_by_key(key).worksheets[0]
 
   random_buzzword = buzzwords.sample
   buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
