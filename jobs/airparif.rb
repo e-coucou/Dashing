44f53b252c79b3b72be4c4bbc74ab5e54ed13fd6
e-coucou <eric@ecoucou.com>
2014-03-18 19:21:15 +0100

    Add airparif

--- jobs/airparif.rb
+++ jobs/airparif.rb
@@ -2,19 +2,32 @@ require "rubygems"
 require "roo"
 require "google_drive"
 
-#key = "0AkHkekTTsBL0dGJ5Ty14YjRud2VBbHhDMXd2UGZPVlE"
 key = "0AkmmwnTmClhodDBHa2xJSDBtTE5LVF9UYktmOUZTeGc"
+
+points = []
+(1..10).each do |i|
+  points << { x: i, y: 0 }
+end
+last_x = points.last[:x]
 
 SCHEDULER.every '2s' do
 	# Logs in.	
 	session = GoogleDrive.login("automate@e-coucou.com", "Penelope75!")
 	ws = session.spreadsheet_by_key(key).worksheets[1]
 
+	# les infos from spreadsheet
 	lieu = ws[1, 2]   # cellule ligne 1  / colonne 2
   	date = ws[1, 3]		# l/c
 	heure = ws[1, 4]		# l/c
 	c_PM10 = ws[1, 6].to_f   # cellule ligne 1  / colonne 2
 	c_NO2 = ws[1, 7].to_f		# l/c
+	
+	# animation du graphique trend
+	points.shift
+	last_x += 1
+	points << { x: last_x, y: c_PM10 }
+
+	send_event('histoPM10', points: points)
 
 #	send_event('ap-val', { current: c_PM10, moreinfo: date+" "+heure })
 	send_event('ap-fo', { title: lieu+"   Maj "+date+" "+heure, moreinfo: 'BASCH' })
